3b50f064a8a461efbdef586eefacab9ec5230075
Maksim Fiodarau <maxsives@gmail.com>
2022-02-28 15:05:40 +0300

    finally got the tests workinggit add .!

--- calc.test.ts
+++ calc.test.ts
@@ -5,4 +5,24 @@ describe("Calculator class:", () => {
   test("add in Typescript", () => {
     expect(calc.addition(10, 2)).toBe(12);
   });
+
+  test("subtraction in Typescript", () => {
+    expect(calc.subtraction(10, 2)).toBe(8);
+  });
+
+  test("multiplication in Typescript", () => {
+    expect(calc.multiplication(10, 2)).toBe(20);
+  });
+
+  test("division in Typescript", () => {
+    expect(calc.division(10, 2)).toBe(5);
+  });
+
+  test("Дробные числа - деление(больше чем)", () => {
+    expect(calc.division(100, 64)).toBeGreaterThan(1.5);
+  });
+
+  test("Дробные числа - вычитание", () => {
+    expect(calc.subtraction(4, 2.4)).toBe(1.6);
+  });
 });
